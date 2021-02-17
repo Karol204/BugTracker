f1d9c154a38bc0f1696d468c5bb2ab66dfefc8bb
karol204 <krogalski204@gmail.com>
2021-02-17 19:20:29 +0000

    Css on project page.

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,22 @@
 // Funkcja odpowiadajaca za fale przy formularzu logowania i rejestracji
 const loginLabels = document.querySelectorAll('.form-control label')
 
+const priorityTypes = document.querySelectorAll('.priorityType')
+
 loginLabels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
+})
+
+console.log(priorityTypes)
+
+priorityTypes.forEach((e) => {
+    console.log(e.innerHTML)
+    if(e.innerHTML === 'Urgent') {
+        e.classList.add('urgent')
+    } else if(e.innerHTML === 'ASAP'){
+        e.classList.add('asap')
+    }
 })
